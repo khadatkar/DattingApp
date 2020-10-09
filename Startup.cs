49b776b03f77c962f2591b005f3c618d35f17b5d
pranay <khadatkar1990@gmail.com>
2020-10-09 17:31:22 +0530

    End of section 3

--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,7 @@ namespace DatingApp
 
 
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +49,8 @@ namespace DatingApp
             }
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
